078337d58d6fd864711e48cdaa151bab0e16bc0f
Kbz-8 <kbz_8.dev@akel-engine.com>
2025-04-04 18:18:11 +0200

    removing files

---
+++ Engine/Runtime/Includes/Renderer/Backends/Vulkan/VulkanRenderer.hpp
@@ -0,0 +1,10 @@
+#ifndef MLK_VULKAN_RENDERER_HPP__
+#define MLK_VULKAN_RENDERER_HPP__
+
+#include <Renderer/Backends/Vulkan/PreCompiled.hpp>
+
+namespace Mlk
+{
+}
+
+#endif
---
+++ Engine/Runtime/Sources/Renderer/Backends/Vulkan/VulkanRenderer.cpp
@@ -0,0 +1,5 @@
+#include <Renderer/Backends/Vulkan/VulkanRenderer.hpp>
+
+namespace Mlk
+{
+}
